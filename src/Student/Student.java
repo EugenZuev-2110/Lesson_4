fe1b1743125a942654b833b170fbe43014b7c6dd
EugenZuev <zhenya.zuev-2110@yandex.ru>
2026-09-20 22:48:07 +0300

    Class Student added

--- src/Student/Student.java
+++ src/Student/Student.java
@@ -1,5 +1,27 @@
 package Student;
 
-public class Student {
+import java.util.List;
 
+public final class Student {
+
+    private final String name;
+    private final List<String> courses;
+
+    public Student(String name, List<String> courses) {
+        this.name = name;
+
+        if (courses != null) {
+            this.courses = List.copyOf(courses);
+        } else {
+            this.courses = List.of();
+        }
+    }
+    
+    public String getName() {
+        return name;
+    }
+
+    public List<String> getCourses() {
+        return courses;
+    }
 }
